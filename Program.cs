4481b05da8ff674484d98e8f24823764f3e9a1fc
gribbib <j.git@motschi.email>
2024-09-28 14:39:22 +0200

    Add solution for day 1 part 2 get digits also from words

--- Program.cs
+++ Program.cs
@@ -6,10 +6,63 @@ Console.WriteLine("Hello, World!");
 using var reader = new StreamReader("input-puzzle-1.txt");
 string line = "";
 int result = 0;
+Dictionary<string, int> numberDictionary = new Dictionary<string, int> {
+    {"zero", 0},
+    {"one", 1},
+    {"two", 2},
+    {"three", 3},
+    {"four", 4},
+    {"five", 5},
+    {"six", 6},
+    {"seven", 7},
+    {"eight", 8},
+    {"nine", 9},
+    {"0", 0},
+    {"1", 1},
+    {"2", 2},
+    {"3", 3},
+    {"4", 4},
+    {"5", 5},
+    {"6", 6},
+    {"7", 7},
+    {"8", 8},
+    {"9", 9},
+    };
 while ((line = reader.ReadLine()) != null){
-    var numberString = new String(line.Where(Char.IsDigit).ToArray());
-    int number = Convert.ToInt32($"{numberString.First()}{numberString.Last()}");
+    Tuple<int,int> firstNumberIndexAndValue = new (-1,-1);
+    Tuple<int,int> lastNumberIndexAndValue = new (-1,-1);
+
+    foreach (var item in numberDictionary)
+    {
+        var list = AllIndexesOf(line, item.Key);
+        if (list.Any()){
+            int minIndex = list.Min();
+            if (minIndex < firstNumberIndexAndValue.Item1 || firstNumberIndexAndValue.Item1 == -1){
+                firstNumberIndexAndValue = new (minIndex, item.Value);
+            }
+            int maxIndex = list.Max();
+            if (maxIndex > lastNumberIndexAndValue.Item1){
+                lastNumberIndexAndValue = new (maxIndex, item.Value);
+            }
+        }
+    }
+    int number = 0;
+    if (firstNumberIndexAndValue.Item2 != -1 && lastNumberIndexAndValue.Item2 != -1){
+        number = int.Parse($"{firstNumberIndexAndValue.Item2}{lastNumberIndexAndValue.Item2}");
+    }
     result += number;
 }
 
 Console.WriteLine(result);
+
+static List<int> AllIndexesOf(string str, string value) {
+    if (String.IsNullOrEmpty(value))
+        throw new ArgumentException("the string to find may not be empty", "value");
+    List<int> indexes = new List<int>();
+    for (int index = 0;; index += value.Length) {
+        index = str.IndexOf(value, index);
+        if (index == -1)
+            return indexes;
+        indexes.Add(index);
+    }
+}
