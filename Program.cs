d41a806dd2d8d9d685144df12b4c9637990a932d
gribbib <j.git@motschi.email>
2024-09-28 14:00:05 +0200

    Add solution for day 1 part 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
+
 Console.WriteLine("Hello, World!");
+using var reader = new StreamReader("input-puzzle-1.txt");
+string line = "";
+int result = 0;
+while ((line = reader.ReadLine()) != null){
+    var numberString = new String(line.Where(Char.IsDigit).ToArray());
+    int number = Convert.ToInt32($"{numberString.First()}{numberString.Last()}");
+    result += number;
+}
+
+Console.WriteLine(result);
