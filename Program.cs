c80be1686e6490e400e8f46b43cfed6a7f545b7a
gribbib <j.git@motschi.email>
2024-09-28 15:50:05 +0200

    Add code for day 2 part 2 get minimum power

--- Program.cs
+++ Program.cs
@@ -3,36 +3,16 @@ using System.Text.RegularExpressions;
 using Microsoft.VisualBasic;
 
 Console.WriteLine("Hello, World!");
-using var reader = new StreamReader("input-puzzle-2.txt");
-string line = "";
-int result = 0;
-    int redThreshold = 12;
-    int greenThreshold = 13;
-    int blueThreshold = 14;
-    int gameCounter = 0;
-while ((line = reader.ReadLine()) != null){
-    gameCounter++;
-    try
-    {
-        CheckColourThreshold(line, "red", redThreshold);
-        CheckColourThreshold(line, "blue", blueThreshold);
-        CheckColourThreshold(line, "green", greenThreshold);
 
-        //if the code runs till here the game is valid and should added to the result
-        result +=gameCounter;
-    }
-    catch (IndexOutOfRangeException){
-        continue;
-    }
-}
+Console.WriteLine(Day2Part2());
 
-Console.WriteLine(result);
-
-static List<int> AllIndexesOf(string str, string value) {
+static List<int> AllIndexesOf(string str, string value)
+{
     if (String.IsNullOrEmpty(value))
         throw new ArgumentException("the string to find may not be empty", "value");
     List<int> indexes = new List<int>();
-    for (int index = 0;; index += value.Length) {
+    for (int index = 0; ; index += value.Length)
+    {
         index = str.IndexOf(value, index);
         if (index == -1)
             return indexes;
@@ -40,7 +20,8 @@ static List<int> AllIndexesOf(string str, string value) {
     }
 }
 
-int Day1(){
+int Day1()
+{
     using var reader = new StreamReader("input-puzzle-1.txt");
     string line = "";
     int result = 0;
@@ -66,31 +47,36 @@ int Day1(){
         {"8", 8},
         {"9", 9},
         };
-    while ((line = reader.ReadLine()) != null){
-        Tuple<int,int> firstNumberIndexAndValue = new (-1,-1);
-        Tuple<int,int> lastNumberIndexAndValue = new (-1,-1);
+    while ((line = reader.ReadLine()) != null)
+    {
+        Tuple<int, int> firstNumberIndexAndValue = new(-1, -1);
+        Tuple<int, int> lastNumberIndexAndValue = new(-1, -1);
 
         foreach (var item in numberDictionary)
         {
             var list = AllIndexesOf(line, item.Key);
-            if (list.Any()){
+            if (list.Any())
+            {
                 int minIndex = list.Min();
-                if (minIndex < firstNumberIndexAndValue.Item1 || firstNumberIndexAndValue.Item1 == -1){
-                    firstNumberIndexAndValue = new (minIndex, item.Value);
+                if (minIndex < firstNumberIndexAndValue.Item1 || firstNumberIndexAndValue.Item1 == -1)
+                {
+                    firstNumberIndexAndValue = new(minIndex, item.Value);
                 }
                 int maxIndex = list.Max();
-                if (maxIndex > lastNumberIndexAndValue.Item1){
-                    lastNumberIndexAndValue = new (maxIndex, item.Value);
+                if (maxIndex > lastNumberIndexAndValue.Item1)
+                {
+                    lastNumberIndexAndValue = new(maxIndex, item.Value);
                 }
             }
         }
         int number = 0;
-        if (firstNumberIndexAndValue.Item2 != -1 && lastNumberIndexAndValue.Item2 != -1){
+        if (firstNumberIndexAndValue.Item2 != -1 && lastNumberIndexAndValue.Item2 != -1)
+        {
             number = int.Parse($"{firstNumberIndexAndValue.Item2}{lastNumberIndexAndValue.Item2}");
         }
         result += number;
     }
-return result;
+    return result;
 }
 
 static void CheckColourThreshold(string line, string colour, int colourThreshold)
@@ -107,4 +93,71 @@ static void CheckColourThreshold(string line, string colour, int colourThreshold
             throw new IndexOutOfRangeException();
         }
     }
+}
+
+static int GetMinimumColourThreshold(string line, string colour)
+{
+    int lastNumber = 1;
+    var list = AllIndexesOf(line, colour);
+    foreach (var index in list)
+    {
+        // get index of space and then get number between the two space
+        int spaceIndex = line.LastIndexOf(" ", index - 2);
+        var numberString = line.Substring(spaceIndex, index - 1 - spaceIndex);
+        var numberColour = int.Parse(numberString);
+
+        if (lastNumber < numberColour){
+            lastNumber = numberColour;
+        }
+    }
+
+    return lastNumber;
+}
+
+int Day2Part1()
+{
+    var reader = new StreamReader("input-puzzle-2.txt");
+    string line = "";
+    int result = 0;
+    int redThreshold = 12;
+    int greenThreshold = 13;
+    int blueThreshold = 14;
+    int gameCounter = 0;
+    while ((line = reader.ReadLine()) != null)
+    {
+        gameCounter++;
+        try
+        {
+            CheckColourThreshold(line, "red", redThreshold);
+            CheckColourThreshold(line, "blue", blueThreshold);
+            CheckColourThreshold(line, "green", greenThreshold);
+
+            //if the code runs till here the game is valid and should added to the result
+            result += gameCounter;
+        }
+        catch (IndexOutOfRangeException)
+        {
+            continue;
+        }
+    }
+
+    return result;
+}
+
+int Day2Part2()
+{
+    var reader = new StreamReader("input-puzzle-2.txt");
+    string line;
+    int result = 0;
+    while ((line = reader.ReadLine()) != null)
+    {
+        int power = GetMinimumColourThreshold(line, "red") * 
+            GetMinimumColourThreshold(line, "blue") *
+            GetMinimumColourThreshold(line, "green");
+
+        //if the code runs till here the game is valid and should added to the result
+        result += power;
+    }
+
+    return result;
 }
