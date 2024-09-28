1fff9535e2598616c726c28e40e971da36092401
gribbib <j.git@motschi.email>
2024-09-28 21:15:53 +0200

    Add day 3 part 2

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualBasic;
 
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine(Day3Part1());
+Console.WriteLine(Day3Part2());
 
 static List<int> AllIndexesOf(string str, string value)
 {
@@ -310,11 +310,180 @@ int Day3Part1()
     return result;
 }
 
+int Day3Part2()
+{
+    var reader = new StreamReader("input-puzzle-3.txt");
+    string line, lastLine = "";
+    int result = 0;
+    List<PartNumber> lastLineNumbers = new List<PartNumber>();
+    List<List<ConsoleItem>> consoleCharacterLines = new List<List<ConsoleItem>>();
+    List<Tuple<int, int, List<PartNumber>>> gearList = new List<Tuple<int, int, List<PartNumber>>>();
+    int lineCounter = 0;
+    while ((line = reader.ReadLine()) != null)
+    {
+        List<ConsoleItem> consoleCharacters = new List<ConsoleItem>();
+        PartNumber? number = null;
+        bool symbolSet = false;
+        int indexCounter = 0;
+        List<PartNumber> currentLineNumbers = new List<PartNumber>();
+        // gear list mit index als id für gear und liste für nummer. am ende dann die gears mit genau 2 nummer suchen und entsprechend berechnen.
+        foreach (var character in line)
+        {
+            if (char.IsNumber(character))
+            {
+                if (number == null)
+                {
+                    number = new PartNumber();
+                    number.Index = indexCounter;
+                }
+                number.ValueString += character;
+            }
+            else if (character != '*')
+            {
+                if (number != null)
+                {
+                    currentLineNumbers.Add(number);
+                    if (symbolSet)
+                    {
+                        gearList.First(g => g.Item1 == number.Index - 1 && g.Item2 == lineCounter).Item3.Add(number);
+                        consoleCharacters.Add(new ConsoleItem { Text = number.ValueString, Colour = ConsoleColor.DarkBlue, Index = number.Index });
+                    }
+                    else
+                    {
+                        //check former line if symbol is available
+                        if (!String.IsNullOrEmpty(lastLine) && gearList.Any())
+                        {
+                            if (lastLine.Substring(number.SearchStartIndex, number.SearchLength).Any(c => c == '*'))
+                            {
+                                var indexes = AllIndexesOf(lastLine.Substring(number.SearchStartIndex, number.SearchLength), "*");
+                                foreach (var i in indexes)
+                                {
+                                    gearList.First(g => g.Item1 == i + number.SearchStartIndex && g.Item2 == lineCounter - 1).Item3.Add(number);
+                                }
+                                consoleCharacters.Add(new ConsoleItem { Text = number.ValueString, Colour = ConsoleColor.Blue, Index = number.Index });
+                            }
+                            else
+                            {
+                                consoleCharacters.Add(new ConsoleItem { Text = number.ValueString, Colour = ConsoleColor.Red, Index = number.Index });
+                            }
+                        }
+                        else
+                        {
+                            consoleCharacters.Add(new ConsoleItem { Text = number.ValueString, Colour = ConsoleColor.Red, Index = number.Index });
+                        }
+                    }
+                }
+                number = null;
+                symbolSet = false;
+                consoleCharacters.Add(new ConsoleItem { Text = character.ToString(), Colour = ConsoleColor.Black, Index = indexCounter });
+            }
+            else
+            {
+                var symbolConsoleColor = ConsoleColor.Yellow;
+                symbolSet = true;
+                var numberList = new List<PartNumber>();
+                gearList.Add(new Tuple<int, int, List<PartNumber>>(indexCounter, lineCounter, numberList));
+                if (number != null)
+                {
+                    numberList.Add(number);
+                    consoleCharacters.Add(new ConsoleItem { Text = number.ValueString, Colour = ConsoleColor.Cyan, Index = number.Index });
+                }
+                if (lastLineNumbers.Any())
+                {
+                    foreach (var n in lastLineNumbers)
+                    {
+                        if (n.IndexInRange(indexCounter))
+                        {
+                            numberList.Add(n);
+                            symbolConsoleColor = ConsoleColor.Green;
+                            consoleCharacterLines.Last().Where(i => i.Index == n.Index).FirstOrDefault().Colour = ConsoleColor.Magenta;
+                            n.Added = true;
+                        }
+                    }
+                }
+                consoleCharacters.Add(new ConsoleItem { Text = character.ToString(), Colour = symbolConsoleColor, Index = indexCounter, Line = lineCounter });
+                number = null;
+            }
+            indexCounter++;
+        }
+
+        if (number != null)
+        {
+            number.LastOnLine = true;
+            currentLineNumbers.Add(number);
+            if (symbolSet)
+            {
+                gearList.First(g => g.Item1 == number.Index - 1 && g.Item2 == lineCounter).Item3.Add(number);
+                consoleCharacters.Add(new ConsoleItem { Text = number.ValueString, Colour = ConsoleColor.DarkBlue, Index = number.Index });
+            }
+            else
+            {
+                //check former line if symbol is available
+                if (!String.IsNullOrEmpty(lastLine))
+                {
+                    if (lastLine.Substring(number.SearchStartIndex, number.SearchLength).Any(c => c == '*'))
+                    {
+                        var indexes = AllIndexesOf(lastLine.Substring(number.SearchStartIndex, number.SearchLength), "*");
+                        foreach (var i in indexes)
+                        {
+                            gearList.First(g => g.Item1 == i + number.SearchStartIndex && g.Item2 == lineCounter - 1).Item3.Add(number);
+                        }
+                        consoleCharacters.Add(new ConsoleItem { Text = number.ValueString, Colour = ConsoleColor.Blue, Index = number.Index });
+                    }
+                    else
+                    {
+                        consoleCharacters.Add(new ConsoleItem { Text = number.ValueString, Colour = ConsoleColor.Red, Index = number.Index });
+                    }
+                }
+                else
+                {
+                    consoleCharacters.Add(new ConsoleItem { Text = number.ValueString, Colour = ConsoleColor.Red, Index = number.Index });
+                }
+            }
+            number = null;
+            symbolSet = false;
+        }
+        lastLine = line;
+        lastLineNumbers = currentLineNumbers;
+        consoleCharacterLines.Add(consoleCharacters);
+        lineCounter++;
+    }
+
+
+    var test = gearList.Where(g => g.Item3.Count < 2);
+    foreach (var t in test)
+    {
+        consoleCharacterLines.SelectMany(l => l).First(c => c.Index == t.Item1 && c.Line == t.Item2).Colour = ConsoleColor.White;
+    }
+
+    foreach (var item in consoleCharacterLines)
+    {
+        foreach (var item2 in item)
+        {
+
+            Console.BackgroundColor = item2.Colour;
+            Console.Write(item2.Text);
+        }
+        Console.WriteLine();
+    }
+
+    var sum = consoleCharacterLines.Sum(l => l
+        .Where(i => i.Colour == ConsoleColor.DarkBlue || i.Colour == ConsoleColor.Blue || i.Colour == ConsoleColor.Cyan || i.Colour == ConsoleColor.Magenta)
+        .Sum(i => long.Parse(i.Text)));
+
+    Console.WriteLine(sum);
+
+    //gear ration sum
+    result = gearList.Where(g => g.Item3.Count == 2).Select(g => g.Item3.First().Value * g.Item3.Last().Value).Sum();
+
+    return result;
+}
 class ConsoleItem
 {
     public string Text { get; set; }
     public ConsoleColor Colour { get; set; }
     public int Index { get; set; }
+    public int Line { get; set; }
 }
 class PartNumber
 {
