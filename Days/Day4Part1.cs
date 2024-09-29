632f4fa41cc66b059cae3c042b098bf33e6d06d4
gribbib <j.git@motschi.email>
2024-09-29 14:52:03 +0200

    Add day 4 part 1

---
+++ Days/Day4Part1.cs
@@ -0,0 +1,24 @@
+
+class Day4Part1 : AbstractDays
+{
+    private string? lastLine;
+    private List<PartNumber> lastLineNotUsedNumbers = new List<PartNumber>();
+    public List<List<ConsoleItem>> ConsoleCharacterLines { get; private set; } = new List<List<ConsoleItem>>();
+
+    public override void DoThings(string line)
+    {
+        int startIndex = line.IndexOf(":") + 2;
+        int seperatorStartIndex = line.IndexOf(" | ");
+        string winningNumbersString = line.Substring(startIndex, seperatorStartIndex - startIndex).Replace("  ", " ").Trim();
+        string givenNumbersString = line.Substring(seperatorStartIndex + 3).Replace("  ", " ").Trim();
+
+        int[] winningNumbers = winningNumbersString.Split(' ').Select(int.Parse).ToArray();
+        int[] givenNumbers = givenNumbersString.Split(' ').Select(int.Parse).ToArray();
+
+        int[] joinedNumbers = (from w in winningNumbers
+            join g in givenNumbers on w equals g
+            select w).ToArray();
+
+        Result += Convert.ToInt32(Math.Pow(2,joinedNumbers.Length - 1));
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,15 +4,18 @@ using Microsoft.VisualBasic;
 
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine(new Day1(){FileName = "puzzle-inputs/input-puzzle-1.txt"}.Run());
-Console.WriteLine(new Day2Part1(){FileName = "puzzle-inputs/input-puzzle-2.txt"}.Run());
-Console.WriteLine(new Day2Part2(){FileName = "puzzle-inputs/input-puzzle-2.txt"}.Run());
+// Console.WriteLine(new Day1(){FileName = "puzzle-inputs/input-puzzle-1.txt"}.Run());
 
-var day31 = new Day3Part1(){FileName = "puzzle-inputs/input-puzzle-3.txt"};
-Console.WriteLine(day31.Run());
-day31.ConsoleCharacterLines.PrintToConsole();
-day31.PrintSumFromConsoleItems();
+// Console.WriteLine(new Day2Part1(){FileName = "puzzle-inputs/input-puzzle-2.txt"}.Run());
+// Console.WriteLine(new Day2Part2(){FileName = "puzzle-inputs/input-puzzle-2.txt"}.Run());
 
-var day32 = new Day3Part2(){FileName = "puzzle-inputs/input-puzzle-3.txt"};
-Console.WriteLine(day32.Run());
-day32.ConsoleCharacterLines.PrintToConsole();
+// var day31 = new Day3Part1(){FileName = "puzzle-inputs/input-puzzle-3.txt"};
+// Console.WriteLine(day31.Run());
+// day31.ConsoleCharacterLines.PrintToConsole();
+// day31.PrintSumFromConsoleItems();
+
+// var day32 = new Day3Part2(){FileName = "puzzle-inputs/input-puzzle-3.txt"};
+// Console.WriteLine(day32.Run());
+// day32.ConsoleCharacterLines.PrintToConsole();
+
+Console.WriteLine(new Day4Part1(){FileName = "puzzle-inputs/input-puzzle-4.txt"}.Run());
